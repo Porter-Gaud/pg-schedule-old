a4b2e93717f5a7633cb12dff645ef9e9f50b0a4d
Tillson Galloway <tillson.galloway@gmail.com>
2015-08-24 23:35:01 -0400

    Fix?

--- public/js/angularCore.js
+++ public/js/angularCore.js
@@ -1,6 +1,6 @@
 var pgSchedule = angular.module('pgSchedule', ['ui.bootstrap', 'cgPrompt']);
 
-pgSchedule.controller('mainController', ['$scope', '$http', '$log', '$interval', function($scope, $http, $log, $interval) {
+pgSchedule.controller('mainController', ['$scope', '$http', '$log', '$interval', '$location', function($scope, $http, $log, $interval) {
   $scope.timeUntil = '';
   $scope.currentDay = '';
   $scope.currentBlock = '';
@@ -65,7 +65,7 @@ pgSchedule.controller('mainController', ['$scope', '$http', '$log', '$interval',
   };
 
   function getApi(endpoint) {
-    return '/api/' + endpoint + ((window.location.href.indexOf('middle') == -1) ? '' : '?middle');
+    return '/api/' + endpoint + (($location.absUrl().indexOf('middle') == -1) ? '' : '?middle');
   }
 
   $scope.getCurrentDay();
