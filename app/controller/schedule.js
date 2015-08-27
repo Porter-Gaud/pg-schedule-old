89f44d5dd7da81f0952b13b8b35eeb70b7ae8818
Tillson Galloway <tillson.galloway@gmail.com>
2015-08-26 23:36:15 -0400

    Bower hates me.

--- app/controller/schedule.js
+++ app/controller/schedule.js
@@ -33,7 +33,7 @@ module.exports.HELP = {
   '/api/timeUntil/': 'The time until next class, based on the server time, which (should be) the same time as school',
   '/api/currentDay/': 'Gives the current day from 0-9 where day A1 is 0, A2 is 1...',
   '/api/currentBlock/': 'Gives the current block (A-G).',
-  '/api/getLunch/': 'Returns the day\'s lunch menu.',
+  '/api/getLunchMenu/': 'Returns the day\'s lunch menu.',
   'Middle School': 'Add ?middle to the API request to get the middle school version of the API.',
   'Want an endpoint we don\'t have? Submit a pull request!': 'https://github.com/ireallydontcare/pg-schedule'
 };
