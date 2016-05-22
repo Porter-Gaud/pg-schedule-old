9199a1a4bb2a0e1470cd10ecc7b7e7ec2e2889b7
Tillson Galloway <tillson.galloway@gmail.com>
2016-05-22 17:19:50 -0400

    End of year schedules

--- app/controller/special.js
+++ app/controller/special.js
@@ -1,22 +1,72 @@
 module.exports.special = [
-  // {
-  //   date: new Date('12/11/2015'),
-  //   day: [
-  //     {'name':'D','start-time':'08:10','end-time':'08:45'},
-  //     {'name':'E','start-time':'08:50','end-time':'08:25'},
-  //     {'name':'F','start-time':'09:30','end-time':'10:05'},
-  //     {'name':'Break','start-time':'10:10','end-time':'10:35'},
-  //     {'name':'G','start-time':'10:35','end-time':'11:10'},
-  //     {'name':'A','start-time':'11:15','end-time':'11:50'},
-  //     {'name':'US B/MS Lunch','start-time':'11:55','end-time':'12:30'},
-  //     {'name':'US Lunch/MS B','start-time':'12:35','end-time':'13:10'},
-  //     {'name':'C','start-time':'13:15','end-time':'13:50'},
-  //     {'name':'Holiday Assembly','start-time':'13:55','end-time':'14:45'},
-  //   ]
-  // }
+  {
+    date: new Date('05/25/2016'),
+    day: [
+      {'name':'G','start-time':'08:10','end-time':'08:40'},
+      {'name':'A','start-time':'08:45','end-time':'09:15'},
+      {'name':'Break','start-time':'09:15','end-time':'09:25'},
+      {'name':'B','start-time':'09:30','end-time':'10:00'},
+      {'name':'C','start-time':'10:05','end-time':'10:35'},
+      {'name':'Unity Assembly','start-time':'10:40','end-time':'11:10'},
+      {'name':'D','start-time':'11:15','end-time':'11:45'},
+      {'name':'E','start-time':'11:50','end-time':'12:20'},
+      {'name':'F','start-time':'12:25','end-time':'12:55'},
+      {'name':'Lunch/Field Day','start-time':'12:55','end-time':'14:30'}
+    ]
+  },
+  {
+    date: new Date('05/26/2016'),
+    day: [
+      {'name':'E','start-time':'08:10','end-time':'08:45'},
+      {'name':'F','start-time':'08:50','end-time':'09:25'},
+      {'name':'Break','start-time':'09:25','end-time':'09:35'},
+      {'name':'G','start-time':'09:40','end-time':'10:15'},
+      {'name':'Community (US Gratitude Assembly)','start-time':'10:20','end-time':'10:50'},
+      {'name':'A','start-time':'10:55','end-time':'11:30'},
+      {'name':'B','start-time':'11:35','end-time':'12:10'},
+      {'name':'Lunch','start-time':'12:10','end-time':'12:45'},
+      {'name':'C','start-time':'12:50','end-time':'13:25'},
+      {'name':'D','start-time':'13:30','end-time':'14:05'}
+    ]
+  },
+  {
+    date: new Date('05/27/2016'),
+    day: [
+      {'name':'F','start-time':'08:00','end-time':'08:25'},
+      {'name':'G','start-time':'08:30','end-time':'08:55'},
+      {'name':'A','start-time':'09:00','end-time':'09:25'},
+      {'name':'Break','start-time':'09:30','end-time':'09:40'},
+      {'name':'B','start-time':'09:45','end-time':'10:10'},
+      {'name':'D','start-time':'10:15','end-time':'10:40'},
+      {'name':'Break','start-time':'10:45','end-time':'10:55'},
+      {'name':'D','start-time':'11:00','end-time':'11:25'},
+      {'name':'E','start-time':'11:30','end-time':'11:55'}
+    ]
+  }
 ];
 
 module.exports.upperOnlySpecial = [
+  {
+    date: new Date('05/31/2016'),
+    day: [
+      {'name':'Math Exam','start-time':'08:00','end-time':'10:15'},
+      {'name':'English Exam','start-time':'10:45','end-time':'13:00'}
+    ]
+  },
+  {
+    date: new Date('06/01/2016'),
+    day: [
+      {'name':'Science Exam','start-time':'08:00','end-time':'10:15'},
+      {'name':'History Exam','start-time':'10:45','end-time':'13:00'}
+    ]
+  },
+  {
+    date: new Date('06/02/2016'),
+    day: [
+      {'name':'Language Exam','start-time':'08:00','end-time':'10:15'},
+      {'name':'Conflict/Make-up Exams','start-time':'10:45','end-time':'13:00'}
+    ]
+  }
 ];
 
 module.exports.middleOnlySpecial = [
