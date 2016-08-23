29aa5dd50f815ac68fd4c5a2d4c68505ea495a8f
Tillson Galloway <tillson.galloway@gmail.com>
2016-08-23 09:22:58 -0400

    Change cookie expiration date

--- public/js/angularCore.js
+++ public/js/angularCore.js
@@ -103,7 +103,7 @@ pgSchedule.controller('mainController', ['$scope', '$http', '$log', '$interval',
     angular.forEach(angular.element.find('.classInput'), function(node) {
       cookieString += (node.id.substring(0, 1)) + '=' + '' + node.value + ',';
     });
-    $cookies.put('schedule', cookieString, {expires: new Date('5/29/2016')});
+    $cookies.put('schedule', cookieString, {expires: new Date('5/29/2018')});
     location.reload(); // Until we get angular-bootstrap working correctly.
   };
 
