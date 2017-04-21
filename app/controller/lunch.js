bc81c7730108ea6b708392552d7d75f06be338fc
Tillson Galloway <tillson.galloway@gmail.com>
2017-04-21 11:24:04 -0400

    Menu fix

--- app/controller/lunch.js
+++ app/controller/lunch.js
@@ -3,40 +3,39 @@ var CronJob = require('cron').CronJob;
 var menu = '';
 
 module.exports.getMenu = function() {
-  return '{}';
-  // if (menu !== '') {
-  //   return menu;
-  // } else {
-  //
-  //   var date = new Date();
-  //   var dateStr = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear().toString().substr(2);
-  //   var schedulePageOptions = {
-  //     host: '206.82.192.168',
-  //     port: 80,
-  //     path: '/v2/menu/' + dateStr + '/app/washingtonhall?key=3D895734-2271-4563-8332-AB943B2E9CAF&siteID=538277458587fc0fd60007ac', // uses the iOS app's api key
-  //     method: 'GET'
-  //   };
-  //
-  //   var req = http.request(schedulePageOptions, function(res) {
-  //     var data = '';
-  //     res.on('data', function(d) {
-  //       data += d;
-  //     });
-  //     res.on('end', function() {
-  //       try {
-  //         data = JSON.parse(data)['meal periods'];
-  //       } catch (e) {
-  //         return '{}';
-  //       }
-  //       if (data[0].name === 'No Meal Service') {
-  //         data.shift();
-  //       }
-  //       menu = data;
-  //       return menu;
-  //     });
-  //   });
-  //   req.end();
-  // }
+  // return '{}';
+  if (menu !== '') {
+    return menu;
+  } else {
+    var date = new Date();
+    var dateStr = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear().toString().substr(2);
+    var schedulePageOptions = {
+      host: '206.82.192.168',
+      port: 80,
+      path: '/v2/menu/' + dateStr + '/app/washingtonhall?key=3D895734-2271-4563-8332-AB943B2E9CAF&siteID=538277458587fc0fd60007ac', // uses the iOS app's api key
+      method: 'GET'
+    };
+
+    var req = http.request(schedulePageOptions, function(res) {
+      var data = '';
+      res.on('data', function(d) {
+        data += d;
+      });
+      res.on('end', function() {
+        try {
+          data = JSON.parse(data)['meal periods'];
+        } catch (e) {
+          return '{}';
+        }
+        if (data[0].name === 'No Meal Service') {
+          data.shift();
+        }
+        menu = data;
+        return menu;
+      });
+    });
+    req.end();
+  }
 };
 
 module.exports.getMenu();
